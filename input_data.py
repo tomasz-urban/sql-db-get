a5024c4fc1210bc63432e5df7931ce5e27da9c7d
tomasz-urban <tomasz.urban.py@gmail.com>
2021-04-08 07:06:51 +0200

    different approach to the problem; querying two databases

---
+++ input_data.py
@@ -0,0 +1,35 @@
+# First database info:
+
+db1_name = "db1 name"  # the database name
+db1_user = "db1 user"  # the database username
+db1_password = "password"  # the database password
+db1_host = "host"  # the database host
+db1_port = "port"  # the default value is 5432
+
+# Sample query
+db1_sql_query = """
+         SELECT emp_no, title FROM titles
+                WHERE emp_no BETWEEN 10001 AND 10004  
+      """
+
+
+# Second database info:
+
+db2_name = "db2 name"  # the database name
+db2_user = "db2 user"  # the database username
+db2_password = "password"  # the database password
+db2_host = "host"  # the database host
+db2_port = "port"  # the default value is 5432
+
+# Sample query
+db2_sql_query = """
+         SELECT emp_no, salary FROM salaries
+                WHERE emp_no BETWEEN 10001 AND 10004
+      """
+
+# Destination folder info:
+
+file_path = ""
+
+
+
--- sql-db-get.py
+++ sql-db-get.py
@@ -1,30 +1,45 @@
-# Import libraries
 import psycopg2
 import pandas as pd
+from datetime import datetime
+import input_data as inp
 
-# Input the necessary information about SQL database connection as follows:
-# db_name - database name
-# db_user - database username
-# db_password - database password
-# db_host - server IP address
-# db_port - connection port (default is 5432)
-db_name = ""
-db_user = ""
-db_password = ""
-db_host = ""
-db_port = ""
 
-conn = psycopg2.connect(database=db_name, user=db_user, password=db_password, host=db_host, port=db_port)
+def db1_configuration():
+    conn = psycopg2.connect(database=inp.db1_name, user=inp.db1_user, password=inp.db1_password,
+                            host=inp.db1_host, port=inp.db1_port)
+    sql_query = pd.read_sql_query(inp.db1_sql_query, conn)
+    df = pd.DataFrame(sql_query, columns=["emp_no", "title"])
+    return df
 
-# Add SQL statement in brackets
-sql = """
 
-       """
+def db2_configuration():
+    conn = psycopg2.connect(database=inp.db2_name, user=inp.db2_user, password=inp.db2_password,
+                            host=inp.db2_host, port=inp.db2_port)
+    sql_query = pd.read_sql_query(inp.db2_sql_query, conn)
+    df = pd.DataFrame(sql_query, columns=["emp_no", "salary"])
+    return df
 
-# The following code reads the SQL statement using connection which were set before
-# and writes the data to CSV file.
-file_1 = pd.read_sql_query(sql, conn)
-conn = None
 
-# Add the path to the destination folder.
-file_1.to_csv(r"file_1.csv")
+def merge_data():
+    merge_df = pd.merge(df_1[["emp_no", "title"]], df_2[["emp_no", "salary"]], on="emp_no")
+    df_merge = pd.DataFrame(merge_df)
+    return df_merge
+
+
+def result_query():
+    output_data = df_3.query("emp_no == 10001")
+    return output_data
+
+
+def save_file():
+    date = datetime.now().strftime("%Y-%m-%d_%I-%M-%S_%p")
+    file_path = inp.file_path
+    saved_file = output_df.to_csv(f'{file_path}{date}.csv')
+    return saved_file
+
+
+df_1 = db1_configuration()
+df_2 = db2_configuration()
+df_3 = merge_data()
+output_df = result_query()
+save_file()
